0c6a048cb7e6de5403011c92c8b3e5869c9fa656
Ryper_Zsolt <akormanyzsolt@gmail.com>
2016-05-03 10:06:01 +0200

    ErrorHandler külön

---
+++ Programs/Skeleton/Skeleton/ErrorHandler.h
@@ -0,0 +1,33 @@
+#pragma once
+
+void getErrorInfo(unsigned int handle) {
+	int logLen;
+	glGetShaderiv(handle, GL_INFO_LOG_LENGTH, &logLen);
+	if (logLen > 0) {
+		char * log = new char[logLen];
+		int written;
+		glGetShaderInfoLog(handle, logLen, &written, log);
+		printf("Shader log:\n%s", log);
+		delete log;
+	}
+}
+
+// check if shader could be compiled
+void checkShader(unsigned int shader, char * message) {
+	int OK;
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &OK);
+	if (!OK) {
+		printf("%s!\n", message);
+		getErrorInfo(shader);
+	}
+}
+
+// check if shader could be linked
+void checkLinking(unsigned int program) {
+	int OK;
+	glGetProgramiv(program, GL_LINK_STATUS, &OK);
+	if (!OK) {
+		printf("Failed to link shader program!\n");
+		getErrorInfo(program);
+	}
+}
--- Programs/Skeleton/Skeleton/Skeleton.cpp
+++ Programs/Skeleton/Skeleton/Skeleton.cpp
@@ -56,44 +56,14 @@ const unsigned int windowWidth = 600, windowHeight = 600;
 // Innentol modosithatod...
 
 #include "vectormath.h"
+#include "ErrorHandler.h"
 
 using namespace std;
 
-//Verziokezelés tesztü
 // OpenGL major and minor versions
 int majorVersion = 3, minorVersion = 0;
 
-void getErrorInfo(unsigned int handle) {
-	int logLen;
-	glGetShaderiv(handle, GL_INFO_LOG_LENGTH, &logLen);
-	if (logLen > 0) {
-		char * log = new char[logLen];
-		int written;
-		glGetShaderInfoLog(handle, logLen, &written, log);
-		printf("Shader log:\n%s", log);
-		delete log;
-	}
-}
 
-// check if shader could be compiled
-void checkShader(unsigned int shader, char * message) {
-	int OK;
-	glGetShaderiv(shader, GL_COMPILE_STATUS, &OK);
-	if (!OK) {
-		printf("%s!\n", message);
-		getErrorInfo(shader);
-	}
-}
-
-// check if shader could be linked
-void checkLinking(unsigned int program) {
-	int OK;
-	glGetProgramiv(program, GL_LINK_STATUS, &OK);
-	if (!OK) {
-		printf("Failed to link shader program!\n");
-		getErrorInfo(program);
-	}
-}
 
 class Shader
 {
